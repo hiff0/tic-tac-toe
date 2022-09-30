56cc75c103ee0b79d2974c4b2fcf04f03acac493
Vlad Petrov <hiffchanel@gmail.com>
2022-09-30 18:17:54 +0300

    add field and components

---
+++ tic-tac-toe/src/components/Square.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export class Square extends React.Component {
+    constructor(props) {
+        super(props); //вызвает родительский конструктор React.Component
+        this.state = {
+            value: null,
+        }
+    }
+
+    render() {
+        return (
+            <button className="square"
+                onClick={() => { this.setState({ value: 'X' }) }}>
+                {this.state.value}
+            </button>
+        );
+    }
+}
