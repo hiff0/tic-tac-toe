ef93867184896c56f680c4d67e1f45d591895394
Vlad Petrov <hiffchanel@gmail.com>
2022-10-03 20:14:44 +0300

    add game history function

--- tic-tac-toe/src/components/Game.jsx
+++ tic-tac-toe/src/components/Game.jsx
@@ -10,11 +10,12 @@ export class Game extends React.Component {
                 squares: Array(9).fill(null), //задаем начальное состояние для для сохранения истории
             }],
             xIsNext: true,
+            stepNumber: 0,
         }
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -26,19 +27,38 @@ export class Game extends React.Component {
                 squares: squares,
             }]),
             xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         let status;
         if (winner) {
-            status = 'Winner: ' + winner;
+            status = 'Победитель: ' + winner;
         } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
         }
+
+        const move = history.map((_step, move) => {
+            const desc = move ?
+                'Перейти к ходу №' + move :
+                'К началу игры';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
 
         return (
             <div className="game">
@@ -49,8 +69,8 @@ export class Game extends React.Component {
                     />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{move}</ol>
                 </div>
             </div>
         );
